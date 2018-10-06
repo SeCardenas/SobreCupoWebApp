d05d0b94c94c1e8124734895a3d1c9a094f3ae3b
Julián Manrique <ja.manrique@uniandes.edu.co>
2018-10-05 19:41:14 -0500

    Refactoring code

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,73 +8,117 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: new Date()
+      time: new Date(),
+      freeClassrooms: []
     };
   }
 
+  computeFreeClassrooms() {
+
+    const freeClassrooms = [];
+
+    //integer time format (same as course's time format) i.e. [1350, 0743, 1822]
+    const now = this.state.time.getHours() * 100 + this.state.time.getMinutes();
+
+    console.log(this.props.dateClassrooms);
+
+    if (this.props.dateClassrooms) {
+      console.log(typeof(this.props.dateClassrooms.classrooms));
+      
+      for (const clasroom of this.props.dateClassrooms.classrooms) {
+
+        let isFree = true;
+        let minutesLeft = 'Todo el día';
+
+        for (const schedule of clasroom.schedules) {
+
+          const start = parseInt(schedule.start);
+          const end = parseInt(schedule.end);
+
+          if (start <= now && now < end) {
+            isFree = false;
+            break;
+          }
+
+          if (start > now) {
+            const startHour = parseInt(schedule.start.substr(0, 2));
+            const startMinute = parseInt(schedule.start.substr(-2));
+            const diff = (startHour - this.state.time.getHours()) * 60 + startMinute - this.state.time.getMinutes();
+            if (minutesLeft === 'Todo el día' || diff < minutesLeft) {
+              minutesLeft = diff;
+            }
+          }
+        }
+        if (isFree && clasroom.name !== '.' && clasroom.name !== '.NOREQ') {
+
+          let freeClasroom = {
+            name: clasroom.name,
+            minutesLeft
+          };
+
+          freeClassrooms.push(freeClasroom);
+        }
+      }
+    }
+
+    return freeClassrooms;
+
+  }
+
+  updateClassrooms() {
+    const ch = new Date(2018, 10, 5, parseInt(this.hour.value), parseInt(this.min.value), 20, 20);
+    this.setState({ time: ch });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ time: new Date() });
+    }, 60000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
-    const hour = this.state.time.getHours()*100 + this.state.time.getMinutes();
-    console.log(hour);
+
+    const freeClassrooms = this.computeFreeClassrooms();
+
     return (
       <div>
         <h1>Welcome!</h1>
         <span>{this.state.time.getMinutes()}</span>
+        <p>Hours</p>
+        <input ref={ref => this.hour = ref} type="text" />
+        <p>Minutes</p>
+        <input ref={ref => this.min = ref} type="text" />
+        <button onClick={() => this.updateClassrooms()}>Update</button>
         <ul>
-          {this.props.classrooms.map(c => {
-            return c.clasrooms.map(cl => {
-              
-              let isFree = true;
-              let minutesLeft = 'Todo el día';
-              for(let i = 0; i<cl.schedules.length; i++) {
-                const start = parseInt(cl.schedules[i].start);
-                const end = parseInt(cl.schedules[i].end);
-                if(start <= hour && end > hour) {
-                  isFree = false;
-                  break;
-                }
-                if(start > hour) {
-                  const startHour = parseInt(cl.schedules[i].start.substr(0,2));
-                  const startMinute = parseInt(cl.schedules[i].start.substr(-2));
-                  const diff = (startHour-this.state.time.getHours())*60 + startMinute - this.state.time.getMinutes();
-                  if(minutesLeft === 'Todo el día' || diff < minutesLeft) minutesLeft = diff;
-                }
-              }
-              if(isFree && cl.classroom !== '.' && cl.clasroom !== '.NOREQ') {
-                return (
-                  <li key={cl.classroom}>
-                    {cl.classroom} {minutesLeft}
-                  </li>
-                );
-              }
-            });
-          })}
+          {freeClassrooms.map(free =>
+            <li key={free.name}>
+              {free.name}: {free.minutesLeft}
+            </li>
+          )}
         </ul>
       </div>
     );
   }
 
-  componentDidMount() {
-    this.interval = setInterval(() => this.setState({ time: new Date() }), 60000);
-  }
-  
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
 }
 
 App.propTypes = {
-  classrooms: PropTypes.array.isRequired
+  dateClassrooms: PropTypes.array.isRequired
 };
 
 export default withTracker(() => {
   let d = new Date();
   let dd = d.getDate();
-  let mm = d.getMonth()+1;
+  let mm = d.getMonth() + 1;
   let yy = d.getFullYear().toString().substr(-2);
-  if(dd<10) dd = '0'+dd;
-  if(mm<10) mm = '0'+mm;
-  console.log(dd+'-'+mm+'-'+yy+'.json');
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+  console.log(dd + '-' + mm + '-' + yy + '.json');
   return {
-    classrooms: Classrooms.find({date: dd+'-'+mm+'-'+yy+'.json'}).fetch()
+    dateClassrooms: Classrooms.findOne({ date: dd + '-' + mm + '-' + yy + '.json' })
   };
 })(App);
