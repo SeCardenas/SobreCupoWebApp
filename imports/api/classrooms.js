c4d744b3bf5e524ce8b79d5069b7d365e3f80652
Julián Manrique <ja.manrique@uniandes.edu.co>
2018-10-05 20:56:56 -0500

    Adjustments to new DB schema

--- imports/api/classrooms.js
+++ imports/api/classrooms.js
@@ -11,13 +11,31 @@ if (Meteor.isServer) {
     let yy = d.getFullYear().toString().substr(-2);
     if(dd<10) dd = '0'+dd;
     if(mm<10) mm = '0'+mm;
-    return Classrooms.find({date: dd+'-'+mm+'-'+yy+'.json'});
+    return Classrooms.find({date: dd+'-'+mm+'-'+yy});
   });
 }
 
 Meteor.methods({
   'classrooms.reportOccupied'(day, classroom, from, to, timestamp) {
     //day: string dd-mm-yy, classroom: string, from: string hhmm, to: string hhmm, timestamp: number
+  },
+  'classrooms.getClassroomSchedules'(day, reqClassroom){
+    
+    const dateInfo = Classrooms.findOne({date: day});
+    
+    if(dateInfo){
+      let searchedClassroom = null;
+      for(const classroom of dateInfo.classrooms){
+        if(classroom.name.includes(reqClassroom)){
+          searchedClassroom = classroom;
+        }
+      }
+
+      return searchedClassroom.schedules;
+    }
+    else{
+      return new Error('Could not find register for that day in the DB');
+    }
   },
   'classrooms.reportFree'(day, classroom, from, to, timestamp) {
     //day: string dd-mm-yy, classroom: string, from: string hhmm, to: string hhmm, timestamp: number
